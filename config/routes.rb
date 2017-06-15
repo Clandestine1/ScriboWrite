024a3332ba7095d70512550dc675086d55757733
Clandestine1 <Clandestine.abyss@gmail.com>
2017-06-14 21:11:15 -0400

    Removed duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+ 
+get ':user_name/edit', to: 'profiles#edit', as: :edit_profile  
   devise_for :users, :controllers => { registrations: 'registrations' }  
-
+get ':user_name', to: 'profiles#show', as: :profile  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :posts do  
   resources :comments
